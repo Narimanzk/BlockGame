b442c58514b8dcc443596a1e92107ea3c3911004
TudorG10 <gurau.tudor@yahoo.com>
2019-03-03 14:21:09 -0500

    basic ui code

--- src/ca/mcgill/ecse223/block/view/Block223Page.java
+++ src/ca/mcgill/ecse223/block/view/Block223Page.java
@@ -2,6 +2,16 @@ package ca.mcgill.ecse223.block.view;
 
 import javax.swing.JFrame;
 
-public class Block223Page extends JFrame{
+public class Block223Page{
+	public JFrame frame;
 
+	
+	public Block223Page() {
+		initialize();
+	}
+	
+	private void initialize() {
+		frame = new JFrame();
+
+	}
 }
